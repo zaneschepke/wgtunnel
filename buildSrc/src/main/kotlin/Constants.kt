e395740c71491c424805a3bae2d3af21e751bd8f
Zane Schepke <zanecschepke@gmail.com>
2024-01-19 20:52:11 -0500

    fix: create config not saving
    Fixes bug where creating a config from scratch was failing to save
    
    Closes #93 #96 #89

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -1,12 +1,12 @@
 object Constants {
-    const val VERSION_NAME = "3.3.3"
+    const val VERSION_NAME = "3.3.4"
     const val JVM_TARGET = "17"
-    const val VERSION_CODE = 33300
+    const val VERSION_CODE = 33400
     const val TARGET_SDK = 34
     const val MIN_SDK = 26
     const val APP_ID = "com.zaneschepke.wireguardautotunnel"
     const val APP_NAME = "wgtunnel"
-    const val COMPOSE_COMPILER_EXTENSION_VERSION = "1.5.7"
+    const val COMPOSE_COMPILER_EXTENSION_VERSION = "1.5.8"
 
 
     const val STORE_PASS_VAR = "SIGNING_STORE_PASSWORD"
