911ed140e0bc7f1124fbf27fa83aa1641604fdb6
Zane Schepke <zanecschepke@gmail.com>
2024-09-19 19:09:33 -0400

    fix: restart tunnel on boot with aovpn
    closes #292
    
    also fixed bug where pausing auto tunnel did not pause pinger

--- app/src/main/java/com/zaneschepke/wireguardautotunnel/ui/AppViewModel.kt
+++ app/src/main/java/com/zaneschepke/wireguardautotunnel/ui/AppViewModel.kt
@@ -4,15 +4,18 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavHostController
 import com.zaneschepke.wireguardautotunnel.data.repository.AppDataRepository
+import com.zaneschepke.wireguardautotunnel.module.IoDispatcher
 import com.zaneschepke.wireguardautotunnel.service.tunnel.TunnelService
 import com.zaneschepke.wireguardautotunnel.util.Constants
 import com.zaneschepke.wireguardautotunnel.util.extensions.TunnelConfigs
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.plus
 import xyz.teamgravity.pin_lock_compose.PinManager
 import javax.inject.Inject
 
@@ -21,8 +24,9 @@ class AppViewModel
 @Inject
 constructor(
 	private val appDataRepository: AppDataRepository,
-	private val tunnelService: TunnelService,
+	tunnelService: TunnelService,
 	val navHostController: NavHostController,
+	@IoDispatcher private val ioDispatcher: CoroutineDispatcher,
 ) : ViewModel() {
 
 	private val _appUiState = MutableStateFlow(AppUiState())
@@ -42,12 +46,12 @@ constructor(
 			)
 		}
 			.stateIn(
-				viewModelScope,
+				viewModelScope + ioDispatcher,
 				SharingStarted.WhileSubscribed(Constants.SUBSCRIPTION_TIMEOUT),
 				_appUiState.value,
 			)
 
-	fun setTunnels(tunnels: TunnelConfigs) = viewModelScope.launch {
+	fun setTunnels(tunnels: TunnelConfigs) = viewModelScope.launch(ioDispatcher) {
 		_appUiState.emit(
 			_appUiState.value.copy(
 				tunnels = tunnels,
@@ -55,7 +59,7 @@ constructor(
 		)
 	}
 
-	fun onPinLockDisabled() = viewModelScope.launch {
+	fun onPinLockDisabled() = viewModelScope.launch(ioDispatcher) {
 		PinManager.clearPin()
 		appDataRepository.appState.setPinLockEnabled(false)
 	}
