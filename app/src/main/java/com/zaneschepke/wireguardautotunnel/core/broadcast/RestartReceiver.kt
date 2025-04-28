2fc0a1a394f87cb5a2a6850463f6f84772065d6f
Zane Schepke <dev@zaneschepke.com>
2025-04-28 15:04:24 -0400

    fix: remove screen on intent

--- app/src/main/java/com/zaneschepke/wireguardautotunnel/core/broadcast/RestartReceiver.kt
+++ app/src/main/java/com/zaneschepke/wireguardautotunnel/core/broadcast/RestartReceiver.kt
@@ -32,7 +32,6 @@ class RestartReceiver : BroadcastReceiver() {
         Timber.d("RestartReceiver triggered with action: ${intent.action}")
         // screen on for Android TV only to help with sleep shutdowns
         val isTv = context.isRunningOnTv()
-        if (intent.action == Intent.ACTION_SCREEN_ON && !isTv) return
         if (intent.action == Intent.ACTION_USER_PRESENT && !isTv) return
         serviceManager.updateTunnelTile()
         serviceManager.updateAutoTunnelTile()
